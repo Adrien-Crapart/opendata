9a2225bc53421601db8e1e9c0430ba730d844085
Adrien <adrien.crapart@gmail.com>
2023-12-09 21:12:19 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// Votre logique JavaScript pour interagir avec les couches sélectionnées
+
+$(document).ready(function () {
+  // Code initialisé lorsque le document est prêt
+  // Vous pouvez ajouter ici la logique pour manipuler les couches sélectionnées
+});
